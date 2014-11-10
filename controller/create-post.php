547b2fecce7f93cd1a0dcef7e6c0437d948e0119
Michael Festekjian <festekjianmichael@gmail.com>
2014-11-10 10:39:49 -0800

    video 21

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,10 @@
 <?php
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+    //the $title is to get the title from the database 
     
     echo "<p>title: $title</p>";
+    //this echos out the title above 
     echo "<p>post: $post</p>";
+    //also with this is to echo out the post above
+    //sends the information to the database
