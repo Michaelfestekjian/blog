278233306b147fa8559da79b7ef3ae5a300964b9
Michael Festekjian <festekjianmichael@gmail.com>
2014-11-13 08:54:44 -0800

    PHP - Blog - 24 Querying our Database to Submit a Blog Post

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,10 +1,24 @@
 <?php
+require_once(__DIR__ . "/../model/database.php");
+//creats a connection to the database 
+//using the directory
+
+
+$connection = new mysqli($host, $username, $password, $database);
+//
+//new database
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     //the $title is to get the title from the database 
     
-    echo "<p>title: $title</p>";
-    //this echos out the title above 
-    echo "<p>post: $post</p>";
-    //also with this is to echo out the post above
-    //sends the information to the database
+$query = $connection->query("INSERT INTO posts SET title = '$title'. post = '$post'");
+    
+if($query) {
+    echo "<p>sucessfully inserted post: $title</p>";
+
+}
+else{
+echo "<p>$connection->error</p>";
+}
+    
+    $connection->close();
--- view/header.php
+++ view/header.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<meta name="viewport" sssssssscontent="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
 
 <html>
     <head>
